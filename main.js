5cd2555dcbc9840ab51c89bff216d8e6114609bc
THANUJ-IMAYAVARAMBAN31 <varamban2007@gmail.com>
2025-12-02 16:06:20 +0530

    Add functionality for color boxes and greeting

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById("submit").onclick = function () {
+    let username = document.getElementById("username").value;
+    document.getElementById("header").textContent = "Hello, " + username + "!";
+};
+function resetBoxes() {
+    document.getElementById("redBox").style.backgroundColor = "white";
+    document.getElementById("blueBox").style.backgroundColor = "white";
+    document.getElementById("greenBox").style.backgroundColor = "white";
+    document.getElementById("yellowBox").style.backgroundColor = "white";
+}
+document.getElementById("redBox").onclick = function() {
+    resetBoxes();
+    this.style.backgroundColor = "red";
+};
+
+document.getElementById("blueBox").onclick = function() {
+    resetBoxes();
+    this.style.backgroundColor = "blue";
+};
+
+document.getElementById("greenBox").onclick = function() {
+    resetBoxes();
+    this.style.backgroundColor = "green";
+};
+
+document.getElementById("yellowBox").onclick = function() {
+    resetBoxes();
+    this.style.backgroundColor = "yellow";
+};
